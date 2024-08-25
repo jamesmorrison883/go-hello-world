d4cfa6a626cf3c537be94758197fa2f530c756aa
jamesmorrison883 <jamesmorrison883@gmail.com>
2024-08-25 15:22:03 -0500

    fix comment errors that reference non-existent variables

--- pointers.go
+++ pointers.go
@@ -1,25 +1,25 @@
 package main
 
 func pointerExamples() {
-	// start with a simple (long form) declaration of a variable, i.
+	// start with a simple (long form) declaration of a variable, x.
 	var x = 42
 	println("value of x:", x)
 
 	// declare a pointer variable, ptr, that is uninitialized.
 	var ptr *int
 
-	// Initialize ptr by assigning the address of i to it, using the 'addressof' operator
+	// Initialize ptr by assigning the address of x to it, using the 'addressof' operator
 	ptr = &x
 
 	//print the value held at the memory address stored in ptr, using the derefrence operator
 	println("value of *ptr:", *ptr)
 
-	// change the value of i by changing the value at the address stored in ptr, using the derefrence operator
+	// change the value of x by changing the value at the address stored in ptr, using the derefrence operator
 	*ptr = 21
 
 	//print the value of ptr, which is an address.
 	println("value of ptr:", ptr)
 
-	//print the value of i, which has been changed by changing the value at the address stored in p
+	//print the value of x, which has been changed by changing the value at the address stored in ptr
 	println("value of x:", x)
 }
